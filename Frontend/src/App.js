3959bc88e324305aef8948c476873f352ed46e36
Lucas <lucas.san.77@gmail.com>
2020-03-29 15:28:42 -0300

    add frontend

---
+++ Frontend/src/App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+
+import Routes from './routes';
+
+import './global.css';
+
+function App() {
+   
+  return (
+    <Routes />
+    );
+}
+
+export default App;
