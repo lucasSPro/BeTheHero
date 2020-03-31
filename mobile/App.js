379af74fdddf4efe6c6d89d18f6606cca2639413
Lucas <lucas.san.77@gmail.com>
2020-03-31 00:26:45 -0300

    initial commit on mobile

---
+++ mobile/App.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable react/jsx-filename-extension */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>É isso ai!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#7159c1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: '#fff',
+    fontSize: 21,
+    fontWeight: 'bold',
+  },
+});
